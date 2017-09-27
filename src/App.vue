a3889c734f3a9f65f775c87018f13f6ca083f061
wslhytxwd <wslhytxwd@gmail.com>
2017-09-27 21:29:22 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">
+    <router-view></router-view>-->
+    <basic></basic>
+  </div>
+</template>
+
+<script>
+  import basic from './components/basic.vue'
+
+  export default {
+    name: 'app',
+    components:{
+      basic
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/basic.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello" v-bind:title="msg">
+    <p v-text="hello"></p>
+    <p v-html="hello"></p>
+    <h1>{{ msg }}</h1>
+    <p>{{num + 1}}</p>
+    <p>{{status ? '是' : '否'}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to demo1',
+      hello:'<p>world</p>',
+      num:1,
+      status:true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
